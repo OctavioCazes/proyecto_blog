77e44f12224ffc107004a0924070c9c0bf676801
Octavio Cazes <octaviohcazes@gmail.com>
2022-08-19 12:12:19 -0300

    agregando boton a noticias, para ver cada noticia en particular

--- proyecto_base/blog/urls.py
+++ proyecto_base/blog/urls.py
@@ -5,7 +5,7 @@ from .views import *
 app_name='blog'
 
 urlpatterns = [
-    path('', BlogView.as_view(template_name='blog.html'), name='blog'),
+    path('<int:id>', BlogView.as_view(template_name='templates_blog/blog.html'), name='blog'),
     path('crear', CrearNoticias.as_view(), name='crear'),
     path('actualizar/<str:slug>/', Actualizar.as_view(), name='actualizar'),    
     path('eliminar/<str:slug>/', Eliminar.as_view(), name='eliminar'),
